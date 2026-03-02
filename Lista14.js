530133476e738cb4b55e5388c66953062956cf73
mariajesus01102630-blip <maria.jesus01102630@sesisenaipr.org.br>
2026-03-02 12:39:13 +0000

    ListadeExercicio

---
+++ Lista14.js
@@ -0,0 +1,3 @@
+// 14 criar um programa que mostre um desconto de 5% para compras maires que r$100
+// 15 criar um programa que classifique uma pessoa como criança, adolecente ou adulto de acordo coom a idade
+// 16 criar um programa que mostre qua tipo de trianguo
